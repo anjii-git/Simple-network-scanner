ee9c697d6437f3447167fa81811fe6148d274e02
anjii-git <anjithav865@gmail.com>
2025-12-31 19:43:59 +0530

    Add files via upload

---
+++ simple network scanner/scanner.py
@@ -0,0 +1,14 @@
+import os
+
+# change this if your network is different
+network = "192.168.1"
+
+print("Scanning network...\n")
+for i in range(1, 20):   
+    ip = network + str(i)
+    response = os.system("ping -n 1 " + ip + ">nul")
+
+    print("checking:",ip)
+    print("Device found:", ip)
+
+print("\nScan finished")    
